81f6350367118d8a03f7d103dfd728b94f07571e
Chris Williams <chris@bingosnet.co.uk>
2009-07-20 13:38:07 +0100

    Prepare for a CPAN development release

--- lib/CPANPLUS/Config/YACSmoke.pm
+++ lib/CPANPLUS/Config/YACSmoke.pm
@@ -4,7 +4,7 @@ use strict;
 use File::Spec;
 use vars qw($VERSION);
 
-$VERSION = '0.42';
+$VERSION = '0.43_01';
 
 sub setup {
   my $conf = shift;
--- lib/CPANPLUS/Dist/YACSmoke.pm
+++ lib/CPANPLUS/Dist/YACSmoke.pm
@@ -20,7 +20,7 @@ use Config::IniFiles;
 
 use vars qw($VERSION);
 
-$VERSION = '0.42';
+$VERSION = '0.43_01';
 
 use constant DATABASE_FILE => 'cpansmoke.dat';
 use constant CONFIG_FILE   => 'cpansmoke.ini';
