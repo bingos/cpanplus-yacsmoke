615a2226a1ab7dd2124dd820097f48ec36cc08a2
Chris Williams <chris@bingosnet.co.uk>
2009-08-25 18:15:22 +0100

    Prepare for a CPAN release

--- lib/CPANPLUS/YACSmoke.pm
+++ lib/CPANPLUS/YACSmoke.pm
@@ -35,7 +35,7 @@ our %EXPORT_TAGS = (
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT    = ( @{ $EXPORT_TAGS{'default'} } );
 
-$VERSION = '0.43_06';
+$VERSION = '0.43_07';
 
 {
   my %Checked;
